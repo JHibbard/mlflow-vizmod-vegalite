a7c08ba14742f9d7ef59f53ab7ed6f235af1bd96
Hibbard, James <James.Hibbard@seattlechildrens.org>
2020-10-12 15:25:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# mlflow_vegastyle
+
+
+# Structure of a Style
+.
+├── README.md
+├── setup.py
+└── src
+    └── mlflow_vega
+        └── styles
+        └── ggplot2.
+        
+.
+├── README.md
+├── setup.py
+└── src
+    └── mlflow_vega
+        └── __init__.py
+        └── styles
---
+++ src/mlflow_vismod/styles/vegalite.py
@@ -0,0 +1,69 @@
+"""
+
+"""
+# Standard Libraries
+import os
+import json
+import io
+import pathlib
+import pandas as pd
+from typing import Optional, Dict
+
+# External Libraries
+import mlflow.pyfunc
+import altair_viewer
+
+
+class Style(mlflow.pyfunc.PythonModel):
+    """
+        local_model_path=local_model_path,
+        model_dir_subpath=model_dir_subpath,
+
+
+
+        # self.local_model_path = kwargs['local_model_path']
+        # self.model_dir_subpath = kwargs['model_dir_subpath']
+    """
+    def __init__(
+            self,
+            artifact_uri,
+    ):
+        self.artifact_uri = artifact_uri
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        # return f'Style(local_model_path={self.local_model_path}, model_dir_subpath={self.model_dir_subpath})'
+        return self.artifact_uri
+
+    def _repr_html_(self):
+        path = os.path.join(self.artifact_uri, 'viz/viz/vizzy.html')
+        with open(path, 'r') as html:
+            data = html.read()
+        return data
+
+    def display(self, model_input):
+        pass
+
+    @staticmethod
+    def save(model, path):
+        model.save(path)
+
+    """
+    def display(self, *kwargs):
+        local_spec_path = os.path.join(self.local_model_path, self.model_dir_subpath, 'spec.json')
+        local_conf_path = os.path.join(self.local_model_path, self.model_dir_subpath, 'config.json')
+        with open(local_spec_path, 'r') as file:
+            spec = json.load(file)
+
+        with open(local_conf_path, 'r') as file:
+            config = json.load(file)
+
+        return altair_viewer.display(spec)
+
+    def get_raw_io(self):
+        path = pathlib.Path(self.local_model_path, self.model_dir_subpath)
+        paths = [p for p in path.glob('*') if p.is_file()]
+        return {str(p).replace(self.local_model_path, ''): io.BytesIO(open(p, 'rb').read()) for p in paths}
+    """
